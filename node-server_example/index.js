944130c406489dbc179144b554bf3ab9d423b533
Alejandro <orta.sanz.93@gmail.com>
2017-11-11 11:42:23 +0100

    Added Nodemoon package

--- node-server_example/index.js
+++ node-server_example/index.js
@@ -1,5 +1,32 @@
 const http = require('http')
 
 const app = new http.Server((req, res) => {
-  
+  console.log('New request')
+
+  const pets= [
+    {
+      name: 'Java',
+      birthdate: '12/11/14',
+      age: 4,
+      vaccinate: true,
+      city: 'Segovia'
+    },
+    {
+      name: 'Mika',
+      birthdate: '04/04/10',
+      age: 7,
+      vaccinate: true,
+      city: 'Huelva'
+    }
+  ]
+
+  if(req.method === 'GET' && req.url === '/pet') {
+    res.writeHead(200, {
+      'Content-type': 'application/json'
+    })
+
+    res.write(JSON.stringify(pets))
+  }
 })
+
+app.listen(3000)
